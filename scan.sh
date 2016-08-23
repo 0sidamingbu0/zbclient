09fdd91e9fff732e0f6bf3d8f1aa227e38070c84
0sidamingbu0 <wangyangidol@163.com>
2016-08-23 10:11:21 +0800

    update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
-read -p "input ip first 1 byte:"  first
-read -p "input ip first 2 byte:"  second
-read -p "input ip first 3 byte:"  third
-read -p "input start 4 byte:"  fourStart
-read -p "input stop 4 byte:"  fourStop
+read -p "input ip 1 byte: "  first
+read -p "input ip 2 byte: "  second
+read -p "input ip 3 byte: "  third
+read -p "input start 4 byte: "  fourStart
+read -p "input stop 4 byte: "  fourStop
+  echo $(date +%Y-%m-%d_%H:%M:%S)
+  echo $(date +%Y-%m-%d_%H:%M:%S) > result.log
 while [ $fourStart -le $fourStop ]
 do
-	echo $first.$second.$third.$fourStart:8888
-	curl $first.$second.$third.$fourStart:8888 --connect-timeout 0.5
-	echo 
+	
+  result=
+	result=$(curl $first.$second.$third.$fourStart:8888 -s --connect-timeout 0.5)
+   if [ ! "$result" ]; then  
+    echo $first.$second.$third.$fourStart:8888 NULL 
+  else  
+    echo $first.$second.$third.$fourStart:8888 $result
+    echo $first.$second.$third.$fourStart:8888 $result >> result.log
+  fi  
+	
 	fourStart=`expr $fourStart + 1`
 done
